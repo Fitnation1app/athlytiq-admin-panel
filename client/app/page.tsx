99a5b389f64b60d6e5fed12ca647dfdb649e18e5
Ruslan622 <sunbeebxy@gmail.com>
2025-06-02 16:11:58 +0600

    redirect login page bugs

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,6 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function DashboardPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("authToken");
-    if (!token) {
-      router.replace("/login");
-    }
-  }, [router]);
-
-  return <div>Dashboard content</div>;
+export default function Page() {
+  redirect("/login");
+  return null;
 }
